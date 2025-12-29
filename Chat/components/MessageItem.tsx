e0f1baae0ab1b06793200396068e86dc4adae88f
Rukafuu <lucas.frischeisen@gmail.com>
2025-12-29 04:20:19 -0300

    fix: Remove remarkGfm from MessageItem.tsx

--- Chat/components/MessageItem.tsx
+++ Chat/components/MessageItem.tsx
@@ -151,7 +151,6 @@ export const MessageItem: React.FC<MessageItemProps> = ({
                         />
                     ) : (
                         <ReactMarkdown 
-                            remarkPlugins={[remarkGfm]}
                             components={{
                                  code({node, inline, className, children, ...props}: any) {
                                     const match = /language-(\w+)/.exec(className || '');
