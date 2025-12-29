1c8027f275761b4b0c3c71e919e87e7990aa5ca7
Rukafuu <lucas.frischeisen@gmail.com>
2025-12-29 01:45:12 -0300

    chore: Add frontend version log to verify deployment update

--- Chat/index.tsx
+++ Chat/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import './i18n';
 import App from './App';
 
+console.log('[LIRA] Frontend v2.5 - Buffer Fix & HF Key Hardcoded');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
